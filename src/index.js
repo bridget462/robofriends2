f2e2087eb919833f441159f3f2201a281c607bc2
bridget462 <bridges.dev.create@gmail.com>
2020-09-23 20:43:01 +0900

    cardlist created

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from "react-dom";
 
 // custom components
 import "./index.css";
-import Card from "./Card";
+import CardList from "./CardList";
+
+// data
 import { robots } from "./robots";
 
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 
-ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
