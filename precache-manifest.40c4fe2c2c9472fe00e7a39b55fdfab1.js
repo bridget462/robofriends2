ff112518ff89484a410c0518094afc354e62db7f
bridget462 <bridges.dev.create@gmail.com>
2020-09-23 23:37:55 +0900

    Updates

---
+++ precache-manifest.40c4fe2c2c9472fe00e7a39b55fdfab1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62784e8decc5247fee041ea81eae9db2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df81501abe9d035ea9ba",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4a1e91252b1e533c6218",
+    "url": "/static/css/main.86c2b84d.chunk.css"
+  },
+  {
+    "revision": "df81501abe9d035ea9ba",
+    "url": "/static/js/2.288be516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.288be516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1e91252b1e533c6218",
+    "url": "/static/js/main.1b187855.chunk.js"
+  },
+  {
+    "revision": "f045811a459dcdaaec05",
+    "url": "/static/js/runtime-main.516fafdc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
