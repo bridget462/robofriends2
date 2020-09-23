11cc5824f376b7b74d29d3d5f88f29ea95442240
bridget462 <bridges.dev.create@gmail.com>
2020-09-23 22:43:45 +0900

    style updated

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./SearchBox.css";
 
 const SearchBox = ({ searchChange }) => {
   return (
     <div>
       <input
-        className="pa3 ba b--green bg-lightest-blue"
+        className="pa3 ba b--white bg-transparent hover-white"
         type="search"
         placeholder="search robots here!"
         onChange={searchChange}
